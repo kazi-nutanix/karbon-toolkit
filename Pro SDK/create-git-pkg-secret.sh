b3807025934e052636df8f60e8582ac227565dda
kazi ahmed <kazi.ahmed@nutanix.com>
2021-08-08 23:38:51 -0400

    updated data

--- Pro SDK/create-git-pkg-secret.sh
+++ Pro SDK/create-git-pkg-secret.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # this script is test and verfied against karbon 2.2.1 only
 set -e
-
+source $SERVER_CONFIG/data.cfg
 now=$(date)
-userName=kazi-nutanix
-secretName=git-docker
-token=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
+userName=$CNTR_RGSTR_USER
+secretName=cntr-registry-secret
+token=$CNTR_RGSTR_PASSWD
 echo "Generating secret configuration $now"
 base64Token=$(echo -n "$userName:$token" | base64)
 base64DkrCfgToken=$(echo -n  '{"auths":{"ghcr.io":{"auth":'\"$base64Token\"'}}}' | base64 -w 0)
