3c07a16529b1faed3b567522d3083bb57c53d600
kazi ahmed <kazi.ahmed@nutanix.com>
2021-08-10 17:59:09 -0400

    added ns parameter to secret creation script

--- Pro SDK/create-git-pkg-secret.sh
+++ Pro SDK/create-git-pkg-secret.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 # this script is test and verfied against karbon 2.2.1 only
 set -e
+if [ ! -z "$1" ]; then
+    echo "Using namespace $1"
+else
+    echo "A namespace name is needed"
+    exit 1
+fi
 source $SERVER_CONFIG/data.cfg
 now=$(date)
 userName=$CNTR_RGSTR_USER
-secretName=cntr-registry-secret
+secretName=git-docker
 token=$CNTR_RGSTR_PASSWD
 echo "Generating secret configuration $now"
 base64Token=$(echo -n "$userName:$token" | base64)
@@ -19,4 +25,4 @@ data:
   .dockerconfigjson: $base64DkrCfgToken
 EOF
 echo "Creating secret $secretName in the cluster at $now"
-kubectl create -f deployments/git-pkg-secret.yaml
+kubectl create -f deployments/git-pkg-secret.yaml -n $1
